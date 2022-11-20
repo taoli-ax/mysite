f6d337649f35f4da2667abb97066e594bb9fb116
tolle <deuta-tao@outlook.com>
2022-11-20 20:20:37 +0800

    explore templates grammar ,detail of article

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import index,year_archive
+from .views import (
+    index,
+    ArticleListView,
+    ArticleDetailView
+)
 
 urlpatterns = [
     path('', index, name='index'),
-    path('international/', year_archive, name='international'),
+    path('articlelist/', ArticleListView.as_view(), name='articlelist'),
+    path('articledetail/<article_id:article>',ArticleDetailView.as_view(),name='articledetail'),
 ]
--- news/views.py
+++ news/views.py
@@ -5,13 +5,28 @@ from django.views import generic
 
 # Create your views here.
 def index(request):
-    context={'msg':' Hello World! '}
-    return render(request,'news/index.html',context)
+    context = {'msg': ' Hello World! '}
+    return render(request, 'news/index.html', context)
 
 
 class ArticleListView(generic.ListView):
-        model=Article
-        queryset=Article.objects.filter(header_line__isnull=False)
-        context = {'articles': queryset}
-        template_name='news/year_archive.html'
+    model = Article
+    template_name = 'news/article_list.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['msg'] = 'Article List'
+        return context
+
+    def get_queryset(self):
+        return Article.objects.all()
+
+
+class ArticleDetailView(generic.DetailView):
+    model = Article
+    template_name = 'new/article_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context=super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['article']=Article.objects.filter(id=self.pk_url_kwarg)
+        return context
